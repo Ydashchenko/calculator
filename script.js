6f0264e6678b65a0afb1d27ef911506eb688c0a8
madjonga <madjonga123@gmail.com>
2023-04-18 02:00:19 +0300

    Extend point and figure functionality

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-let firstNumber = '0'
-let secondNumber = null
+let firstNumber = ''
+let secondNumber = ''
 let operation = ''
 
 let currentNumber = document.getElementById('current-number')
-currentNumber.textContent = firstNumber
+currentNumber.textContent = '0'
 
 let figures = document.querySelectorAll('.figure')
 figures.forEach(figure => figure.addEventListener('click', function(e) {
-    if (firstNumber === '0') {
+    if (firstNumber === '') {
         firstNumber = figure.textContent
         currentNumber.textContent = figure.textContent
-    } else {
+    } else if (firstNumber !== '' && expression.textContent === '') {
         firstNumber = firstNumber + figure.textContent
         currentNumber.textContent = firstNumber
+    } else {
+        secondNumber = secondNumber + figure.textContent
+        currentNumber.textContent = secondNumber
     }
 }))
 
 const point = document.querySelector('.point')
 point.addEventListener('click', function(e) {
-    if (!firstNumber.includes('.')) {
-        currentNumber.textContent += '.'
-        firstNumber = currentNumber.textContent
+    if (expression.textContent === '') {
+        if (!currentNumber.textContent.includes('.')) {
+            currentNumber.textContent += '.'
+            firstNumber = currentNumber.textContent
+        }
+    } else {
+        if (!currentNumber.textContent.includes('.')) {
+            currentNumber.textContent += '.'
+            secondNumber = currentNumber.textContent
+        }
     }
 })
 
 const expression = document.getElementById('full-expression')
 let operations = document.querySelectorAll('.opt-btn')
 operations.forEach(operation => operation.addEventListener('click', function(e) {
-    if (secondNumber == null) {
+    if (secondNumber == '') {
         expression.textContent = currentNumber.textContent + operation.textContent
         firstNumber = currentNumber.textContent
     } //else {
